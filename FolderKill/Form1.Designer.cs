67da6603a284bcd258f5189b3660367c4a4e5adf
Jake Van Buren <jakevb8@gmail.com>
2014-04-29 15:12:42 -0400

    Initial version

---
+++ FolderKill/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace FolderKill
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDirectory = new System.Windows.Forms.TextBox();
+            this.btnDirectory = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnKillDirectory = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // txtDirectory
+            // 
+            this.txtDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDirectory.Location = new System.Drawing.Point(13, 13);
+            this.txtDirectory.Name = "txtDirectory";
+            this.txtDirectory.Size = new System.Drawing.Size(251, 20);
+            this.txtDirectory.TabIndex = 0;
+            this.txtDirectory.Text = "C:\\FolderToDelete";
+            // 
+            // btnDirectory
+            // 
+            this.btnDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDirectory.Location = new System.Drawing.Point(264, 10);
+            this.btnDirectory.Name = "btnDirectory";
+            this.btnDirectory.Size = new System.Drawing.Size(35, 23);
+            this.btnDirectory.TabIndex = 1;
+            this.btnDirectory.Text = "...";
+            this.btnDirectory.UseVisualStyleBackColor = true;
+            this.btnDirectory.Click += new System.EventHandler(this.btnDirectory_Click);
+            // 
+            // btnKillDirectory
+            // 
+            this.btnKillDirectory.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnKillDirectory.Location = new System.Drawing.Point(243, 46);
+            this.btnKillDirectory.Name = "btnKillDirectory";
+            this.btnKillDirectory.Size = new System.Drawing.Size(56, 23);
+            this.btnKillDirectory.TabIndex = 2;
+            this.btnKillDirectory.Text = "Kill";
+            this.btnKillDirectory.UseVisualStyleBackColor = true;
+            this.btnKillDirectory.Click += new System.EventHandler(this.btnKillDirectory_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(13, 45);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(224, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.progressBar1.TabIndex = 3;
+            this.progressBar1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 75);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnKillDirectory);
+            this.Controls.Add(this.btnDirectory);
+            this.Controls.Add(this.txtDirectory);
+            this.Name = "Form1";
+            this.Text = "Folder Kill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDirectory;
+        private System.Windows.Forms.Button btnDirectory;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btnKillDirectory;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ FolderKill/Form1.cs
@@ -0,0 +1,91 @@
+﻿using Delimon.Win32.IO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FolderKill
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDirectory_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtDirectory.Text = folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private async void btnKillDirectory_Click(object sender, EventArgs e)
+        {
+            try
+            {               
+                DirectoryInfo directoryInfo = new DirectoryInfo(txtDirectory.Text);
+                if (directoryInfo.Exists)
+                {
+                    btnKillDirectory.Enabled = false;
+                    btnKillDirectory.Text = "Killing...";
+                    await DeleteFilesAsync(directoryInfo, directoryInfo);
+                    btnKillDirectory.Enabled = true;
+                    btnKillDirectory.Text = "Kill";
+                }              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private async Task DeleteFilesAsync(DirectoryInfo root, DirectoryInfo currentDirectory)
+        {
+            await Task.Run(() =>
+            {
+                DeleteFiles(root, currentDirectory);
+                root.Delete(true);
+            });
+        }
+        private void DeleteFiles(DirectoryInfo root, DirectoryInfo currentDirectory)
+        {
+            FileInfo[] files = currentDirectory.GetFiles();
+            if (files.Length > 0)
+            {
+                //Application.DoEvents();
+            }
+            foreach (FileInfo fileToMove in files)
+            {
+                try
+                {
+                    fileToMove.Delete();
+                    //string reallyLongPath = @"\\?\" + currentDirectory.FullName + "\\" + fileToMove.Name;
+                    //string destination = root.FullName + "\\" + fileToMove.Name;
+                    //CopyFile(reallyLongPath, destination, false);
+                    //FileInfo fileToDelete = new FileInfo(root.FullName + "\\" + fileToMove.Name);
+                    //fileToDelete.Delete();
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+
+            foreach (DirectoryInfo directoryInfo in currentDirectory.GetDirectories())
+            {
+                DeleteFiles(root, directoryInfo);
+            }
+        }
+
+        //[DllImport("kernel32.dll", CharSet = CharSet.Unicode)]
+        //static extern bool CopyFile(string lpExistingFileName, string lpNewFileName, bool bFailIfExists);
+    }
+}
